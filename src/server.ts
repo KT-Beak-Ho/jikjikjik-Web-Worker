be59a33fe3f8f3fc5b0695224de815b81b5c04a6
JOyagdol <pool021224@gmail.com>
2025-09-08 21:26:25 +0900

    update env structure : subtree

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
-import "dotenv/config";
+import dotenv from "dotenv";
+dotenv.config({ path: './jikjikjik-Web-Env/.env' });
 import Fastify from "fastify";
 import cors from "@fastify/cors";
 import helmet from "@fastify/helmet";
